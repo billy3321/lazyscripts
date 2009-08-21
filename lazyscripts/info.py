2b302419a2e1be8f0627a9f6f3d3364e23538c9a
billy3321 <billy3321@gmail.com>
2009-08-21 22:24:19 +0800

    Add distinguish if lsb_release not exist

--- lazyscripts/info.py
+++ lazyscripts/info.py
@@ -2,6 +2,7 @@
 # -*- encoding=utf8 -*-
 # @author '2009 Hsin Yi Chen (陳信屹) <ossug.hychen@gmail.com>'
 from commands import getoutput
+import os
 
 def get_distro():
     """
@@ -9,16 +10,19 @@ def get_distro():
 
     @return tuple (distrobution name, distrobution version code)
     """
-    cmd = 'lsb_release'
-    name = getoutput(cmd+' -is')
-    if name in ('Debian','Ubuntu') :
-        code = getoutput(cmd+' -cs')
-    else :
-        code = getoutput(cmd+' -rs')
-
-    if name == 'SUSE LINUX' and code in ('11.0','11.1') :
-        name = 'openSUSE'
+    if os.path.isfile('/usr/bin/lsb_release') or os.path.isfile('/bin/lsb_release'):
+        cmd = 'lsb_release'
+        name = getoutput(cmd+' -is')
+        if name in ('Debian','Ubuntu') :
+            code = getoutput(cmd+' -cs')
+        else :
+            code = getoutput(cmd+' -rs')
     
+        if name == 'SUSE LINUX' and code in ('11.0','11.1','11.2') :
+            name = 'openSUSE'
+    else:
+        name = os.getenv('DISTRO_ID')
+        code = os.getenv('DISTRO_VERSION')
     return (name,code)
 
 def get_distro_name():
