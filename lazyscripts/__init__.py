ee5494203faf372a006d4a0122e43b14756d3df4
billy3321 <billy3321@gmail.com>
2010-03-13 16:11:35 +0800

    reset to hychen/lazyscripts.git

--- lazyscripts/__init__.py
+++
@@ -1,7 +0,0 @@
-
-__author__ = 'Hsin Yi, Chen (hychen) <ossug.hychen@gmail.com>'
-__version__ = '0.1.1'
-__date__ = '2009-01-04'
-
-def get_version():
-	return __version__
--- t/test_detect.py
+++
@@ -1,6 +0,0 @@
-from lazyscripts import info
-
-def test_distro():
-    "test to get the information about current distrobution."
-    _info= info.get_distro() 
-    assert _info == ('Ubuntu','intrepid'), _info
