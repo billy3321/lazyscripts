303f6e6dd51ecac98acd019d2d434c5dcf59fa75
billy3321 <billy3321@gmail.com>
2010-03-20 01:22:41 +0800

    fix syntax error

--- lazyscripts/console.py
+++ lazyscripts/console.py
@@ -95,14 +95,14 @@ def gui_run():
     env.register_workspace()
     env.prepare_runtimeenv()
     env.storageenv()
-    distro = platform.dist()
-    if not distro[0]:
+    distro = platform.dist()[0]
+    if not distro:
         if os.path.exists('/etc/arch-release'):
-            distro[0]='arch'
+            distro = 'arch'
         else:
             print "distribution no supported."
             sys.exit()
-    win_mgr = wm.get_wminfo(distro[0])
+    win_mgr = wm.get_wminfo(distro)
 
     # argument process.
     message_sudo="\"執行'Lazyscripts 懶人包' 會修改系統設定，並會安裝新軟體，所以需要系統管理員權限。 請輸入系統管理密碼，才能繼續執行。(在 Lazyscripts 下，預設這就是你登入系統時所用的密碼。)\""
@@ -137,11 +137,11 @@ def gui_run():
 
     if options.selection_list:
         cmd = 'lzs gui run %s' % (options.selection_list)
-        guisudocmd = wm.make_guisudocmd(distro[0],win_mgr,cmd,message_sudo)
+        guisudocmd = wm.make_guisudocmd(distro,win_mgr,cmd,message_sudo)
 #        cmd = "%s lzs gui run %s" % (prefix, options.selection_list)
     else:
         cmd = 'lzs gui run'
-        guisudocmd = wm.make_guisudocmd(distro[0],win_mgr,cmd,message_sudo)
+        guisudocmd = wm.make_guisudocmd(distro,win_mgr,cmd,message_sudo)
 #        cmd = "%s lzs gui run" % prefix
     os.system(guisudocmd)
 #}}}
