71836c964ae9cb80f78c17d3f81c0fddbe5c9aa7
billy3321 <billy3321@gmail.com>
2010-04-07 15:27:43 +0800

    fix syntax error

--- lazyscripts/env.py
+++ lazyscripts/env.py
@@ -239,7 +239,7 @@ def storageenv(path=None):
     mkexport('LANG'),
     'export DISTRO_ID=%s'
     'export DISTRO_VERSION=%s'
-    'export PLAT_NAME=%s' % (distro version platform_name)
+    'export PLAT_NAME=%s' % (distro, version, platform_name)
     ]
     if not path:
         path = DEFAULT_RUNTIME_ROOT_DIR
