da200f4fa4fc92dbb16e4992b240e2bde8bb56cd
billy3321 <billy3321@gmail.com>
2010-08-12 20:34:48 +0800

    fix an error make lazyscripts can't distinguish language on lazyscripts

--- lazyscripts/env.py
+++ lazyscripts/env.py
@@ -54,7 +54,12 @@ def get_local():
         local = locale.getlocal(locale.LC_ALL)
         if local:
             local = local[0]
-    if not gettext.find('lazyscripts',languages=[local]):
+    localedirs = ['', '/usr/share/locale', '/usr/local/share/locale']
+    find_locale = False
+    for localedir in localedirs:
+        if gettext.find('lazyscripts', localedir=localedir, languages=[local]):
+            find_locale = True
+    if not find_locale:
         local = 'en_US'
     return local
 #}}}
