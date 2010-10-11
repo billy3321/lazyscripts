2dab32651f17ddf5cef245f92122e9ffd7cb2c66
billy3321 <billy3321@gmail.com>
2010-10-11 19:18:50 +0800

    modified the version

--- lazyscripts/__init__.py
+++ lazyscripts/__init__.py
@@ -1,4 +1,4 @@
-__VERSION__ = "0.2.3.6"
+__VERSION__ = "0.2.3.8"
 __WEBURL__ = "http://www.lazyscripts.org/"
 __BUGLIST__ = "https://bugs.launchpad.net/lazyscripts"
 __MAIL__ = "lazyscripts-dev@googlegroup.com"
